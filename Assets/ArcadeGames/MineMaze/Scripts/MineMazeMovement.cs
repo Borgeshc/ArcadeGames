bfc8b495586b363baf599ebb7a28d55d283a14da
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-09-21 12:02:49 -0500

    initial commit
    added incontrol and movement

---
+++ Assets/ArcadeGames/MineMaze/Scripts/MineMazeMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineMazeMovement : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public GameObject cameraObject;
+
+    Vector3 movement;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update ()
+    {
+        transform.Rotate(new Vector3(0, PlayerInput.horizontal2 * rotationSpeed * Time.deltaTime,0));
+        cameraObject.transform.Rotate(new Vector3(-PlayerInput.vertical2 * rotationSpeed * Time.deltaTime, 0, 0));
+
+        movement = new Vector3(PlayerInput.horizontal * speed * Time.deltaTime, 0, PlayerInput.vertical * speed * Time.deltaTime);
+        movement = transform.TransformDirection(movement);
+        rb.velocity = movement;
+    }
+}
---
+++ Assets/ArcadeGames/SharedFiles/Scripts/PlayerInput.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public static float horizontal;
+    public static float vertical;
+    public static float horizontal2;
+    public static float vertical2;
+    public static bool isJumping;
+    public static bool isFiring;
+
+    Controls controls;
+    string saveData;
+
+    void OnEnable()
+    {
+        controls = Controls.CreateWithDefaultBindings();
+    }
+
+    void OnDisable()
+    {
+        controls.Destroy();
+    }
+
+    private void Update()
+    {
+        horizontal = controls.Move.X;
+        vertical = controls.Move.Y;
+        horizontal2 = controls.Look.X;
+        vertical2 = controls.Look.Y;
+
+        isJumping = controls.Jump;
+        isFiring = controls.Fire;
+    }
+
+    void SaveBindings()
+    {
+        saveData = controls.Save();
+        PlayerPrefs.SetString("Bindings", saveData);
+    }
+
+    void LoadBindings()
+    {
+        if (PlayerPrefs.HasKey("Bindings"))
+        {
+            saveData = PlayerPrefs.GetString("Bindings");
+            controls.Load(saveData);
+        }
+    }
+}
